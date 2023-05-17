f909c5a4d7738056701090d6df66be653470f6e8
projesilvestrini <100374678+projesilvestrini@users.noreply.github.com>
2023-05-17 14:27:29 -0300

    Add files via upload

---
+++ exercico 1/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace exercicio1;
+class Program
+{
+    static void Main(string[] args)
+    {
+    Homem Cleber= new Homem();
+    Cao Thor = new Cao();
+    Gato Floquinho = new Gato();
+
+    Console.WriteLine(Cleber.Fala());
+    Console.WriteLine(Thor.Fala());
+    Console.WriteLine(Floquinho.Fala());
+    }   
+        
+}
